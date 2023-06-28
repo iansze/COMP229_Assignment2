f93b7558a28e999a19dec9a3657ea23862b8f6a6
Ian Sze <iansze1124@gmail.com>
2023-06-28 19:31:32 -0400

    up

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const app = require("./backend/app");
-const port = process.env.MONGODB_URI || 3000;
+const port = process.env.PORT || 3000;
 
 app.set("port", port);
 const server = http.createServer(app);
