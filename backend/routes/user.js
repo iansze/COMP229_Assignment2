900af28d588bac6e330b1f973866d56ad271e9ee
Ian Sze <iansze1124@gmail.com>
2023-06-29 09:45:01 -0400

    d

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,14 +4,29 @@ const User = require("../models/user");
 const port = process.env.PORT || 3000;
 
 router.post("/api/register", (req, res, next) => {
-  console.log(port);
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+  });
+  user
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "User created!",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
-router.get("process.env.PORT", (req, res, next) => {
+router.get("/api/list", (req, res, next) => {
   User.find().then((doc) => {
     res.status(200).json({
       message: "sucess",
-      users: doc,
     });
   });
 });
